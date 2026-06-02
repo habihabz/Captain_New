3cf008b64af974fe64b39d0e8221c4315601e45c
Habeeb Rahman <Habeeb.Rahman@a2c.co.uk>
2026-06-02 21:38:10 +0530

    app name changes

---
+++ captain-app/android/app/src/main/kotlin/com/husicaptain/captain_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.husicaptain.captain_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- erp_customer_mobile/android/app/src/main/kotlin/com/husicaptain/erp_customer_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.husicaptain.erp_customer_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
